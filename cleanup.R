93791a2981c59e76fbb1bf55d3f7d4de60de4a66
tan_server <tan@tanho.ca>
2022-04-16 06:41:56 -0400

    fix cleanup

--- cleanup.R
+++ cleanup.R
@@ -10,24 +10,30 @@ suppressPackageStartupMessages({
 
 cleanup <- function(){
   on.exit({
-    POST("https://hc-ping.com/9345d7c2-eddc-4b99-b995-0717fc007e6a/fail",
+    POST("https://hc-ping.com/639523c7-5cfb-4503-b2eb-427959e039b1/fail",
          body = "Failed to cleanup trades from server!")
   })
+
   list_trades <- list.files("storage", recursive = TRUE, full.names = TRUE)
 
-  map_dfr(list_trades, arrow::read_parquet) %>%
-    write_parquet(glue::glue("storage/trades_{Sys.Date()}.parquet"))
+  upload_path <- glue::glue("storage/trades_{as.numeric(Sys.time())}.parquet")
 
-  piggyback::pb_upload(glue::glue("storage/trades_{Sys.time()}.parquet"),
+  map_dfr(list_trades, arrow::read_parquet) %>%
+    write_parquet(upload_path)
+
+  piggyback::pb_upload(upload_path,
                        repo = "dynastyprocess/apps-calculator",
                        tag = "trade_data")
 
-  list.files("storage", recursive = TRUE, full.names = TRUE) %>%
+  list_trades %>%
     fs::file_delete()
+
+  fs::file_delete(upload_path)
 
   on.exit(NULL)
 
-  POST("https://hc-ping.com/9345d7c2-eddc-4b99-b995-0717fc007e6a",
+  POST("https://hc-ping.com/639523c7-5cfb-4503-b2eb-427959e039b1",
        body = glue("Successfully cleaned out local data at {Sys.time()}"))
-
 }
+
+cleanup()
